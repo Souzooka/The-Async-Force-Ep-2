758aee889ba917f5a359b2e7183a333bb6a46795
Dakota <bazookaharwell@gmail.com>
2017-04-08 19:49:41 -1000

    try catch todo

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ function getData(source, property, element, eFunc) {
   const oReq = new XMLHttpRequest();
 
   oReq.addEventListener('load', function() {
+    try {
     const data = JSON.parse(this.responseText);
+    } catch(e) {
+      // display error?
+    }
     eFunc(data, property, element);
   });
   oReq.open('GET', source);
