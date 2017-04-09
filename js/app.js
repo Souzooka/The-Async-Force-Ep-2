85a1e68bf445fd27db26ed7ee9c103de4daa45ba
Dakota <bazookaharwell@gmail.com>
2017-04-08 19:37:48 -1000

    add getData helper function

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function getData(source, property, element, eFunc) {
+  const oReq = new XMLHttpRequest();
+
+  oReq.addEventListener('load', function() {
+    const data = JSON.parse(this.responseText);
+    eFunc(data, property, element);
+  });
+  oReq.open('GET', source);
+  oReq.send();
+}
