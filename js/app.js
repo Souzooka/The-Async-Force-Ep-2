e65740ac575f1f148a00ad5908b914899522398d
Dakota <bazookaharwell@gmail.com>
2017-04-10 10:09:29 -1000

    create makeContentVisible func

--- js/app.js
+++ js/app.js
@@ -39,16 +39,47 @@ function clearInfo() {
 }
 
 function addPlanets(data, element) {
-  console.log('butts')
+
 
 }
 
 function addPeople(data, element) {
+  const name = document.createElement('h2');
+  const gender = document.createElement('p');
 
+  name.innerHTML = data.name;
+  gender.innerHTML = data.gender;
+
+  // Even though the species is another request, we want all of the info
+  // to be shown at one time rather than streamed in
+  name.style.display = 'none';
+  gender.style.display = 'none';
+
+  element.appendChild(name);
+  element.appendChild(gender);
+
+  getData(data.species[0], element, null, addSpecies);
 }
 
 function addStarships(data, element) {
 
+}
+
+function addSpecies(source, data, element, option) {
+  makeContentVisible(element);
+}
+
+function addFilm(source, data, element, option) {
+  makeContentVisible(element);
+}
+
+// for when all data is retrieved
+function makeContentVisible(element) {
+  console.log(element)
+  for (let i = 0; i < element.childNodes.length; ++i) {
+    console.log(i)
+    element.childNodes[i].style.display = 'block';
+  }
 }
 
 function displayError(source) {
@@ -62,7 +93,7 @@ document.querySelector('#requestResourceButton')
   .addEventListener ('click', function() {
     const option = document.querySelector('#resourceType').value;
     const pageId = document.querySelector('#resourceId').value;
-    const container = document.querySelector('contentContainer');
+    const container = document.querySelector('#contentContainer');
     clearInfo();
     getData(`http://swapi.co/api/${option}/${pageId}/`,
             container, option, addDataToPage);
