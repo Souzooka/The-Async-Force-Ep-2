9a37b83c5079c7138e2e1687d986c0d6d76c387d
Dakota <bazookaharwell@gmail.com>
2017-04-10 09:19:14 -1000

    successfully get data from API

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
-function getData(source, property, element, eFunc) {
+function getData(source, element, option, eFunc) {
   const oReq = new XMLHttpRequest();
 
   oReq.addEventListener('load', function() {
-    try {
     const data = JSON.parse(this.responseText);
-    } catch(e) {
-      // display error?
-    }
-    eFunc(data, property, element);
+    eFunc(data, element, option);
   });
   oReq.open('GET', source);
   oReq.send();
 }
 
 document.querySelector('#requestResourceButton')
-  .addEventListener ('onclick', function() {
-    //TODO
+  .addEventListener ('click', function() {
+    const option = document.querySelector('#resourceType').value;
+    const pageId = document.querySelector('#resourceId').value;
+    const container = document.querySelector('contentContainer');
+    getData(`http://swapi.co/api/${option}/${pageId}/`,
+            container, option, addDataToPage);
   });
+
+function addDataToPage(data, element, option) {
+  console.log(data);
+}
